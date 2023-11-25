c38cbaef2384550d201a6029f49eb4615b9ed41a
phenixua <112823718+phenixua@users.noreply.github.com>
2023-11-25 22:15:19 +0200

    2

---
+++ app_instagram/app_auth/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib import messages
+
+from .forms import RegisterForm
+
+
+# Create your views here.
+
+class RegisterView(View):
+    template_name = 'app_auth/register.html'
+    forms_class = RegisterForm
+
+    def get(self, request):
+        return render(request, self.template_name, context={"form": self.forms_class})
+
+    def post(self, request):
+        form = self.forms_class(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            messages.success(request, message=f"Вітаємо {username}. Ваш акаунт успішно створено.")
+            return redirect(to="app_auth:signin")
+        return render(request, self.template_name, context={"form": form})
